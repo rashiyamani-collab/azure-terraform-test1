c27cd7ea2a4b0916636ef9fe545eeb6a6c3bc692
rashiyamani-collab <97337399+rashiyamani-collab@users.noreply.github.com>
2024-09-09 09:15:19 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -17,7 +17,7 @@ provider "azurerm" {
 data "azurerm_client_config" "current" {}
 
 #Create Resource Group
-resource "azurerm_resource_group" "azureterraformtest" {
+resource "azurerm_resource_group" "azureterraformtest3" {
   name     = "statefilecontainer"
   location = "eastus2"
 }
@@ -27,13 +27,13 @@ resource "azurerm_virtual_network" "vnet" {
   name                = "tamops-vnet"
   address_space       = ["192.168.0.0/16"]
   location            = "eastus2"
-  resource_group_name = azurerm_resource_group.azureterraformtest.name
+  resource_group_name = azurerm_resource_group.azureterraformtest3.name
 }
 
 # Create Subnet
 resource "azurerm_subnet" "subnet" {
   name                 = "subnet"
-  resource_group_name  = azurerm_resource_group.azureterraformtest.name
+  resource_group_name  = azurerm_resource_group.azureterraformtest3.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["192.168.0.0/24"]
 }
